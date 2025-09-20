a1407298fa2522963ddd0e78193b9c585f703da0
sujitkhandagale <sujitkh94@outlook.com>
2025-09-20 11:40:40 +0530

    docker compose for mongodb, redis, rabbitmq. eslint added for backend app build on nodejs.

---
+++ backend/eslint.config.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    languageOptions: {
+      sourceType: 'module',
+      ecmaVersion: 'latest',
+      parserOptions: {
+        ecmaFeatures: {
+          globalReturn: true,
+          impliedStrict: true,
+        },
+      },
+    },
+  },
+];
